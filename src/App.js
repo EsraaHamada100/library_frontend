97b1f9672edbdfe4c208e857b2537f229cab5cc3
EsraaHamada100 <esraahamada654@gmail.com>
2023-04-03 22:29:48 +0200

    3-4-2023

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './styles/App.css';
-import Header from './shared/header.js'
+import Header from './shared/Header.js';
+import Footer from './shared/Footer.js'
 function App() {
   return true?(
     <div className="App">
       <Header element1="Books" element2="Orders" element3="Search terms" />
+      <Footer/>
     </div>
   ): (
     <div className="App">
---
+++ src/shared/Footer.js
@@ -0,0 +1,10 @@
+import '../styles/Footer.css';
+const Footer = ()=> {
+  return (
+    <footer className="footer">
+      <p className="footer-text">© 2023 Book Website. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
