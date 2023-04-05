f81ebef097b223461e803bc655b6db7d13ff585c
EsraaHamada100 <esraahamada654@gmail.com>
2023-04-05 16:11:48 +0200

    adding routes

---
+++ src/router.js
@@ -0,0 +1,105 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from './App';
+import NotFound from "./shared/NotFound";
+import UserBooksView from "./user/book_page/UserBooksView";
+export const router = createBrowserRouter(
+
+
+    // If the user is admin he will go there
+    userIsLoggedIn && userIsAdmin ? [
+        {
+            path: "/",
+            element: <App />,
+            children: adminRoutes,
+        },
+        notFoundRoute,
+
+    ]
+        // If the user is Logged in and he is a normal user 
+        : userIsLoggedIn ? [
+            {
+                path: "/",
+                element: <App />,
+                children: userRoutes,
+            },
+            notFoundRoute,
+        ]
+            // If the user is not logged in 
+            : [
+                {
+                    path: "/",
+                    element: <App />,
+                    children: guestRoutes,
+                },
+                {
+                    path: '*',
+                    element: <Login />,
+                }
+            ],
+
+
+
+
+);
+
+const adminRoutes = [
+    {
+        path: '/',
+        element: <NotFound />,
+    },
+    {
+        path: '/books',
+        element: <UserBooksView />,
+    },
+    {
+        path: '/about',
+        element: <AboutPage />,
+    },
+    {
+        path: '/contact',
+        element: <ContactPage />,
+    },
+];
+
+const userRoutes = [
+    {
+        path: '/',
+        element: <NotFound />,
+    },
+    {
+        path: '/books',
+        element: <UserBooksView />,
+    },
+    {
+        path: '/about',
+        element: <AboutPage />,
+    },
+    {
+        path: '/contact',
+        element: <ContactPage />,
+    },
+];
+
+const guestRoutes = [
+    {
+        path: '/',
+        element: <NotFound />,
+    },
+    {
+        path: '/books',
+        element: <UserBooksView />,
+    },
+    {
+        path: '/about',
+        element: <AboutPage />,
+    },
+    {
+        path: '/contact',
+        element: <ContactPage />,
+    },
+];
+
+const notFoundRoute = {
+    path: '*',
+    element: <NotFound />,
+};
--- src/user/book_page/components/BookCard.js
+++ src/user/book_page/components/BookCard.js
@@ -2,17 +2,17 @@ import '../styles/BookCard.css';
 const BookCard = ({book})=>{
     return (
         <div className="book">
-            <div>
+            <div className="date">
                 {/* To get only the date not the time associated with it */}
                 <p>{book.publication_date.split('T')[0]}</p>
             </div>
-            <div>
+            <div className="book-cover">
                 <img 
                     src={book.cover_link}
                     alt={book.book_name} 
                 />
             </div>
-            <div>
+            <div className="book-info">
                 <span>{book.field}</span>
                 <h3>{book.book_name}</h3>
             </div>
